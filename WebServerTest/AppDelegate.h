235e225921a2d1cb706feef89d025159e9ef5296
Dominik Arnhof <d.arnhof@gmail.com>
2012-08-14 13:46:00 +0200

    Initial Commit

---
+++ WebServerTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  WebServerTest
+//
+//  Created by Dominik Arnhof on 14.08.12.
+//  Copyright (c) 2012 HTL Hollabrunn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ WebServerTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebServerTest
+//
+//  Created by Dominik Arnhof on 14.08.12.
+//  Copyright (c) 2012 HTL Hollabrunn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
